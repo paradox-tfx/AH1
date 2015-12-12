2edbef0b7d5dbefe66ad44a567671381074b15b8
paradox-tfx <sobin.ghose@haw-hamburg.de>
2015-12-12 22:33:48 +0100

    working: Starting Another Activity

--- app/src/main/java/oct25/helloworld/FirstActivity.java
+++ app/src/main/java/oct25/helloworld/FirstActivity.java
@@ -1,5 +1,6 @@
 package oct25.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
-public class FristActivity extends AppCompatActivity {
-
+public class FirstActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.firstactivity.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frist);
+        setContentView(R.layout.activity_first);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +28,9 @@ public class FristActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
     }
 
     @Override
@@ -48,5 +53,15 @@ public class FristActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
     }
 }
